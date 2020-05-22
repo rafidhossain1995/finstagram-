d882d40f02de0e9b8d9df42cb1c82b1bf6c94a75
rafidhossain1995 <rafidhossain@pursuit.org>
2020-05-22 19:06:46 -0400

    able to redirect from signup to login page and vice versa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch, Redirect} from "react-router-dom"
+import {Route, Switch} from "react-router-dom"
 // import HomePage from "./Front End/component/HomePage.js"
 import Login from "./Front End/component/Login"
 import SignUp from "./Front End/component/SignUp"
@@ -10,8 +10,13 @@ function App() {
   return (
     <div className="App">
     <Switch>
-      <Route path={"/"} component={SignUp}/>
-      <Route exact path = {"/login"} component={Login}/>
+      <Route exact path = {"/"}>
+        <SignUp/>
+      </Route>
+      <Route path = {"/login"}>
+        <Login/>
+      </Route>
+
     </Switch>
     </div>
   );
