99f747db8874dd16b3fa8d99aff7816942ee2409
rafidhossain1995 <rafidhossain@pursuit.org>
2020-06-01 22:49:48 -0400

    start homepage

--- frontend/src/component/NavBar.js
+++ frontend/src/component/NavBar.js
@@ -2,29 +2,22 @@ import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 import {logout} from "../utility/firebaseFunction"
 import {AuthContext} from "../providers/AuthContext"
+import "../CSS/Navbar.css"
+
 
 
 export default function NavBar(params) {
-    const currentUser = useContext(AuthContext)
-    const display = ()=>{
-        if(currentUser){
-            return <button onClick= {logout}>logout</button>
-        }else{
-            return(
-                <>
-                <NavLink to ={"/signup"}>signup</NavLink>
-                <NavLink to ={"/login"}>login</NavLink>
-                
-                </>
-            )
-        }
-    }
+    
+
+
+
     return(
         <nav>
-            <NavLink to={"/home"}>Home</NavLink>
+            <NavLink to={"/home"} className="home" >Home</NavLink>
             <NavLink to={"/users"}>Show All Users</NavLink>
+            <NavLink to = {"/profle"}>Profile</NavLink>
             <button onClick = {logout}>Logout</button>
-            {display()}
+        
         </nav>
     )
 };
